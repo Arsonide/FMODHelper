a9464f35710e3c92c5d231c9f0181f3719ca492a
Arsonide <arsonide@yahoo.com>
2024-01-06 20:54:36 -0600

    Initial project files.

---
+++ FMODHelper.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+
+namespace FMODHelper;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class FMODHelper : BasePlugin
+{
+    public override void Load()
+    {
+        // FMODHelper startup logic
+        Log.LogInfo($"FMODHelper {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
